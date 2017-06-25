8c8491073ea9a98f4e9a8503203a2abd9a409d18
Sonja Moenkemeyer <smoenkemeyer@zalando-08210.fritz.box>
2017-06-25 19:55:46 +0200

    Emit new Message on createMessage

--- server/server.js
+++ server/server.js
@@ -14,15 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'server',
-    text: 'hey, whats up?',
-    createdAt: 123
-  });
-
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage);
-    socket.emit('newMessage', newMessage);
+    io.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
